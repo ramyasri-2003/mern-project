762a55714244b009c338ab9df742bbf6148aae6b
Ramya Sri <thirumalasettyramyasri21@gmail.com>
2024-03-26 20:48:34 +0530

    User.js modified

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-// const router = express.Router();
+const router = express.Router();
 const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator');
 const Shopuser = require('../../models/Shopuser');
